a962930adc8709f00dac179460e0bc4060a037e5
Muhammad Arif Afandi <arifafandi242@gmail.com>
2023-08-14 20:48:07 +0700

    Update error log

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const httpProxy = require('http-proxy');
-const app = express();
+const fs = require('fs');
 
+const app = express();
 const proxy = httpProxy.createProxyServer();
+
+// Function to log errors to a file
+function logError(error) {
+  const timestamp = new Date().toISOString();
+  const logMessage = `${timestamp}: ${error.stack}\n`;
+
+  fs.appendFile('error.log', logMessage, (err) => {
+    if (err) {
+      console.error('Error writing to error.log:', err);
+    }
+  });
+}
 
 app.use('/', (req, res) => {
   // Spoofed IP address for educational purposes
@@ -14,7 +27,11 @@ app.use('/', (req, res) => {
   const target = req.query.request;
 
   // Proxy the request using the spoofed IP
-  proxy.web(req, res, { target });
+  proxy.web(req, res, { target }, (error) => {
+    console.error('An error occurred:', error);
+    logError(error);
+    res.sendStatus(500); // Send an error response to the client
+  });
 });
 
 app.listen(3000, () => {
